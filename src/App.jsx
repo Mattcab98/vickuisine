9de1f7e611d1f6bd7b57400587260728760cccea
Matias Alvarez <matiasalvarezbustos@gmail.com>
2025-03-10 22:10:28 -0300

    correccion ruta archivo hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './assets/components/header/header.jsx'
-import Hero from './assets/components/hero/hero.jsx'
+import Hero from './assets/components/hero/Hero.jsx'
 
 function App() {
 
